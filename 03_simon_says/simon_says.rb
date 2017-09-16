a1b1a6117d1b78729b1bb0ea7edbfa97fed4eb65
Celestine Ekoh-Ordan <ceo.ehis@outlook.com>
2017-09-17 00:11:47 +0100

    all tests passed!

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,39 @@
 #write your code here
+def simon_says
+  return echo('hello')
+end
+
+def echo(input)
+  return input
+end
+
+
+def shout(input)
+  return input.upcase
+end
+
+def repeat(input, count = 2)
+  return ([input] * count).join ' '
+end
+
+def start_of_word(input, idx)
+  return input[0..idx-1]
+end
+
+def first_word(input)
+  return input.split(' ').first
+end
+
+def titleize(input)
+  input.capitalize!
+  ignores = %w( the of a and over)
+  sentence = input.split
+  sentence.map do |word|
+    if ignores.include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end.join(' ')
+end
+
