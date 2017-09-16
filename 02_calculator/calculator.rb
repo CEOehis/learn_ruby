8b60c2800cdf25b24c941c76be5cab71bc382aaa
Celestine Ekoh-Ordan <ceo.ehis@outlook.com>
2017-09-16 22:31:33 +0100

    all tests passed, plus bonus

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
 #write your code here
+def add(x,y)
+  return x + y
+end
+
+def subtract(x,y)
+  return x - y
+end
+
+def sum(arr)
+  return arr.inject(0){|sum,x| sum + x}
+end
+
+def multiply(x,y)
+  return x * y
+end
+
+def power(x,y)
+  return x ** y
+end
+
+def factorial(num)
+  if (num == 0)
+    return 1
+  end
+  return num * factorial(num - 1)
+end
