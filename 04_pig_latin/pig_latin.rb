d5e3856be2f7aec5240111a3686a4a891df8dd12
Celestine Ekoh-Ordan <ceo.ehis@outlook.com>
2017-09-17 02:30:39 +0100

    pig latin first function definition

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,10 @@
 #write your code here
+def translate(input)
+  letters = input.split("")
+  # ignores = %w( the of a and over)
+  vowels = %w(a,e,i,o,u)
+  if vowels.include?letters.first
+    letters.push("ay")
+  end
+  letters
+end
